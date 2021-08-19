e7a92b1f71c6e28d8331fe6b44476b0e7638d8f0
Octaviotastico <octaviopercivaldi2@gmail.com>
2021-08-19 16:30:17 -0300

    Minor changes

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -1,11 +1,18 @@
+// React and Material UI
 import { Container, Typography } from "@material-ui/core";
+
+// Redux
+import { useSelector } from "react-redux";
+
+// Styles
 import "./Home.scss";
 
 const Home = () => {
+  const { theme } = useSelector((state) => state);
   const user = "Octa"
   return (
-    <Container disableGutters maxWidth={false} className="Home">
-      <Typography className="MainTitle">Wellcome again, {user}</Typography>
+    <Container disableGutters maxWidth={false} className={`Home-${theme}`}>
+      <Typography className={`MainTitle-${theme}`}>Wellcome again, {user}</Typography>
     </Container>
   );
 };
