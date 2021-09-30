352993fb469100d7306d0c1545b3727012dc4823
octaviotastico <octaviopercivaldi2@gmail.com>
2021-09-30 01:23:24 -0300

    Working on learning screen, writting articles, please rewrite everything xd

--- src/Components/PSelect/PSelect.js
+++ src/Components/PSelect/PSelect.js
@@ -1,68 +1,120 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
 import Select from "react-select";
 import "./PSelect.scss";
 
 const PSelect = ({
   options,
-  className,
+  chosenOptions,
+  setChosenOption,
   inputChange,
   customFilter,
-  setChosenOption,
-  chosenOptions,
-  defaultValue,
-  placeholder,
-  multiple,
-  isClearable,
-  containerStyles,
+  defaultValue = null,
+  placeholder = "",
+  multiple = false,
+  isClearable = false,
 }) => {
+
+  const customStyles = {
+    control: () => ({
+      display: 'flex',
+      backgroundColor: '#FCFCFD',
+      borderRadius: 10,
+      borderStyle: "none",
+      fontWeight: 600,
+      minHeight: 40,
+    }),
+    menu: () => ({
+      backgroundColor: '#FCFCFD',
+      borderRadius: 10,
+      borderStyle: "none",
+      color: "#CDD1D9",
+      marginTop: 10,
+    }),
+    option: (_, state) => ({
+      backgroundColor: state.isFocused ? "#E5E7F3" : '#FCFCFD',
+      color: "#35353b",
+      fontWeight: 600,
+      padding: 10,
+      alignItems: "center",
+      justifyContent: "center",
+      alignSelf: "center",
+      justifySelf: "center",
+      borderRadius: 10,
+    }),
+    dropdownIndicator: () => ({
+      color: "#35353b",
+      marginRight: 10,
+      marginLeft: 10,
+    }),
+    clearIndicator: () => ({
+      color: "#35353b",
+      marginRight: 10,
+      marginLeft: 10,
+    }),
+    singleValue: (provided, state) => ({
+      ...provided,
+      opacity: state.isDisabled ? 0.5 : 1,
+      transition: 'opacity 300ms'
+    }),
+    multiValue: (_, state) => ({
+      backgroundColor: "#E5E7F3",
+      justifyContent: "space-between",
+      boxSizing: "border-box",
+      marginTop: 3,
+      marginBottom: 3,
+      marginRight: 3,
+      borderRadius: 10,
+      display: "flex",
+      minWidth: 100,
+      padding: 10,
+    }),
+    multiValueLabel: (provided, state) => ({
+      color: "#35353b"
+    }),
+    menuPortal: () => ({
+      backgroundColor: "green",
+    }),
+  }
+
   return (
-    <Grid container item className="PSelect">
-      <Grid item xs={12} style={containerStyles}>
-        <Select
-          className={className}
-          filterOption={customFilter}
-          onInputChange={inputChange}
-          isClearable={isClearable}
-          hideSelectedOptions
-          options={options}
-          isMulti={multiple}
-          placeholder={placeholder || ""}
-          value={chosenOptions}
-          noOptionsMessage={() =>
-            (multiple && chosenOptions?.length === options.length) ||
-            (!multiple && options?.length === 1)
-              ? "No more options"
-              : "Loading..."
-          }
-          defaultValue={defaultValue || ""}
-          onChange={(item, selectAction) => {
-            const { action } = selectAction;
-            if (!setChosenOption) return;
-            switch (action) {
-              case "clear":
-                setChosenOption([]);
-                break;
-              case "select-option":
-                setChosenOption(item);
-                break;
-              case "remove-value":
-                if (!multiple) setChosenOption(null);
-                else {
-                  const item = selectAction.removedValue;
-                  const aux = chosenOptions.filter(
-                    (elem) => elem.value !== item.value
-                  );
-                  setChosenOption([].concat(aux));
-                }
-                break;
-              default:
-                break;
-            }
-          }}
-        />
-      </Grid>
-    </Grid>
+    <Select
+      styles={customStyles}
+
+      filterOption={customFilter}
+      onInputChange={inputChange}
+      isClearable={isClearable}
+      hideSelectedOptions
+      options={options}
+      isMulti={multiple}
+      placeholder={placeholder || ""}
+      value={chosenOptions}
+      noOptionsMessage={() => "No more options"}
+      defaultValue={defaultValue || ""}
+      onChange={(item, selectAction) => {
+        // Get action type
+        const { action } = selectAction;
+        // If no function provided, return
+        if (!setChosenOption) return;
+
+
+        switch (action) {
+          case "clear":
+            setChosenOption(item);
+            break;
+
+          case "select-option":
+            setChosenOption(item);
+            break;
+
+          case "remove-value":
+            setChosenOption(item);
+            break;
+
+          default:
+            break;
+        }
+      }}
+    />
   );
 };
 
