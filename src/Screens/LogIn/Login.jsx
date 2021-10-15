541297b7b61974863ee4bd5f4f3092b2e0ab8e8d
Octaviotastico <octaviopercivaldi2@gmail.com>
2021-10-15 03:41:50 -0300

    Lots of improvements in Login screen

--- src/Screens/LogIn/Login.jsx
+++ src/Screens/LogIn/Login.jsx
@@ -1,8 +1,14 @@
 // React
 import React from 'react';
 
-// Material
-import { Container, Grid, Typography } from '@material-ui/core';
+// Material UI
+import {
+  Button, Container, Grid, Typography,
+} from '@material-ui/core';
+import { AccountBox, Lock } from '@material-ui/icons';
+
+// Router
+import { navigate } from '../../Router';
 
 // Styles
 import './Login.scss';
@@ -10,15 +16,60 @@ import './Login.scss';
 const Login = () => (
   <Container disableGutters maxWidth={false} className="Login">
     <Grid className="LeftGrid">
+
       <Grid className="LoginBox">
         <Typography className="LoginTitle">
           Login
         </Typography>
+
+        <Grid className="InputsContainer">
+
+          <Grid className="InputWrapper">
+            <Grid className="IconContainer">
+              <AccountBox className="UsernameIcon" />
+            </Grid>
+            <input
+              className="Input"
+              placeholder="Username"
+              autoComplete={false}
+              type="text"
+            />
+          </Grid>
+
+          <Grid className="InputWrapper">
+            <Grid className="IconContainer">
+              <Lock className="PasswordIcon" />
+            </Grid>
+            <input
+              className="Input"
+              placeholder="Password"
+              autoComplete={false}
+              type="password"
+            />
+          </Grid>
+        </Grid>
+
+        <Button className="LoginButton">
+          Login
+        </Button>
       </Grid>
-      <Typography>
-        If you don&apos;t have an account, you can SignUp.
+
+      <Typography className="HelpText">
+        If you don&apos;t have an account, you can
+        {' '}
+        <span
+          role="button"
+          tabIndex={0}
+          className="SignUp"
+          onClick={() => navigate('/signup')}
+          onKeyDown={() => navigate('/signup')}
+        >
+          SignUp
+        </span>
+        .
       </Typography>
     </Grid>
+
     <Grid className="RightGrid">
       <Typography className="WellcomeTitle">
         Wellcome to EpicCMS! :)
